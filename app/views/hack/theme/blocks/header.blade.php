bab9e609e3bf0a15f7f9b7886b9b885653b82f53
rokasl <rokas@helix.lt>
2014-10-11 04:34:35 +0100

    added bootstrap/css and updated index page

--- app/views/hack/theme/blocks/header.blade.php
+++ app/views/hack/theme/blocks/header.blade.php
@@ -11,7 +11,7 @@
         <link href="/assets/bootstrap-3.2.0-dist/css/bootstrap.min.css" rel="stylesheet">
 
         <!-- Custom styles for this template -->
-        <link href="justified-nav.css" rel="stylesheet">
+        <link href="assets/css/justified-nav.css" rel="stylesheet">
 
         <!-- HTML5 shim and Respond.js IE8 support of HTML5 elements and media queries -->
         <!--[if lt IE 9]>
