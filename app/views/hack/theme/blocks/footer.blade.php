d71f7b790cd1707b5edae0177a2bae1648eb9fdc
rokasl <rokas@helix.lt>
2014-10-11 08:51:53 +0100

    added bootstrap/css and updated index page

--- app/views/hack/theme/blocks/footer.blade.php
+++ app/views/hack/theme/blocks/footer.blade.php
@@ -1,17 +1,32 @@
-    <!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
-    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.1/jquery.min.js"></script>
-    <!-- Include all compiled plugins (below), or include individual files as needed -->
-    <script src="assets/js/bootstrap.min.js"></script>
 
 <!--FANCYBOX JS -->
   <script type="text/javascript">
 
  $(document).ready(function() {
-     $(".iframe").fancybox({
-         type: 'iframe'
-     });
+ 	$(".various").fancybox({
+ 		maxWidth	: 800,
+ 		maxHeight	: 600,
+ 		fitToView	: false,
+ 		width		: '70%',
+ 		height		: '70%',
+ 		autoSize	: false,
+ 		closeClick	: false,
+ 		openEffect	: 'none',
+ 		closeEffect	: 'none'
+ 	});
  });
   </script>
+
+
+
+
+
+
+    <!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.1/jquery.min.js"></script>
+    <!-- Include all compiled plugins (below), or include individual files as needed -->
+    <script src="assets/js/bootstrap.min.js"></script>
+
 
 
 
