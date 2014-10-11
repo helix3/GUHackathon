46d278d0c22a37f43c63862da79d97f5f2884c8d
rokasl <rokas@helix.lt>
2014-10-11 22:54:23 +0100

    sdasdasdas

--- app/views/hack/theme/info.blade.php
+++ app/views/hack/theme/info.blade.php
@@ -139,7 +139,7 @@
                             <form method="get" action="http://www.google.com/search">
                             <div style="padding:4px;width:20em;">
                              <table class="table table-hover"><tr><td>
-                              <input type="text" name="q" size="25" maxlength="255" value="Terrorist attack"/>
+                              <input type="text" name="q" size="25" maxlength="255" value="<?= $data->date['date'].', '.$data->city.', '.$data->country.' Terrorist attack' ?>" />
                               <input type="submit" value="Google Search" /></td></tr>
                               </table> </div> </form>
 
