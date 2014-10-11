6a43e4b776f5b6fcef2a893a4bd6ceaea181dc8c
rokasl <rokas@helix.lt>
2014-10-11 23:02:07 +0100

    sdasdasdas

--- app/views/hack/theme/info.blade.php
+++ app/views/hack/theme/info.blade.php
@@ -52,7 +52,7 @@
                                                                       display: block;
                                                                       position: relative;
                                                                       ">
-                                                                      <div class="thumbnail">
+                                                                      <div class="thumbnail" style="width:100%">
                                  <table class="table table-hover">
                                           <thead>
                                             <tr>
@@ -139,7 +139,7 @@
                             <form method="get" action="http://www.google.com/search">
                             <div style="padding:4px;width:20em;">
                              <table class="table table-hover"><tr><td>
-                              <input type="text" name="q" size="50" maxlength="255" value="<?= $data->date['date'].', '.$data->city.', '.$data->country.' Terrorist attack' ?>" />
+                              <input type="text" name="q" size="60" maxlength="255" value="<?= $data->date['date'].', '.$data->city.', '.$data->country.' Terrorist attack' ?>" />
                               <input type="submit" value="Google Search" /></td></tr>
                               </table> </div> </form>
 
