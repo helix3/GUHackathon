f8d74b8db19f76afd7c437142bf8a8c6bf49bec9
rokasl <rokas@helix.lt>
2014-10-11 05:24:06 +0100

    added bootstrap/css and updated index page

--- app/views/hack/theme/blocks/header.blade.php
+++ app/views/hack/theme/blocks/header.blade.php
@@ -28,11 +28,10 @@
               <h3 class="text-muted">Project name</h3>
               <ul class="nav nav-justified">
                 <li class="active"><a href="#">Home</a></li>
-                <li><a href="#">Projects</a></li>
-                <li><a href="#">Services</a></li>
-                <li><a href="#">Downloads</a></li>
+                <li><a href="#">Statistics</a></li>
+                <li><a href="#">Unknown</a></li>
                 <li><a href="#">About</a></li>
-                <li><a href="#">Contact</a></li>
+
               </ul>
             </div>
 
