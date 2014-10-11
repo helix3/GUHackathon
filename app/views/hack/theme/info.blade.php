5b16355922e4706d1074e60b55b8ac324c8e17e5
rokasl <rokas@helix.lt>
2014-10-11 21:29:35 +0100

    added bootstrap/css and updated index page

--- app/views/hack/theme/info.blade.php
+++ app/views/hack/theme/info.blade.php
@@ -135,7 +135,7 @@
                         lat: <?= $data->lat ?>,
                         lng: <?= $data->long ?>,
                         infoWindow: {
-                            content: '<p><h4><a class="various" data-fancybox-type="iframe" href="/resource/'+entry._id+'">'+entry.attack_type+'</a></h4>  Date: '+entry.date.date+'<br> Location: '+entry.city+', '+entry.country+' </p>'
+                            content: '<p><h4><a  href="/resource/'+entry._id+'">'+entry.attack_type+'</a></h4>  Date: '+entry.date.date+'<br> Location: '+entry.city+', '+entry.country+' </p>'
                         }
                     });
 
