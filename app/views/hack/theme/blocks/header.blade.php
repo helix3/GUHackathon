e3abb6101b0fe4cfd2b22606d9f16629df564278
rokasl <rokas@helix.lt>
2014-10-11 03:53:21 +0100

    added bootstrap/css and updated index page

--- app/views/hack/theme/blocks/header.blade.php
+++ app/views/hack/theme/blocks/header.blade.php
@@ -1,1 +1,40 @@
-<?php
+    <!DOCTYPE html>
+    <html lang="en">
+      <head>
+        <meta charset="utf-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+
+        <title>ALPHA PRO</title>
+
+        <!-- Bootstrap core CSS -->
+        <link href="public/assets/bootstrap-3.2.0-dist/css/bootstrap.min.css" rel="stylesheet">
+
+        <!-- Custom styles for this template -->
+        <link href="justified-nav.css" rel="stylesheet">
+
+        <!-- HTML5 shim and Respond.js IE8 support of HTML5 elements and media queries -->
+        <!--[if lt IE 9]>
+          <script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
+          <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
+        <![endif]-->
+      </head>
+
+
+      <body>
+      <div class="container">
+
+            <div class="masthead">
+              <h3 class="text-muted">Project name</h3>
+              <ul class="nav nav-justified">
+                <li class="active"><a href="#">Home</a></li>
+                <li><a href="#">Projects</a></li>
+                <li><a href="#">Services</a></li>
+                <li><a href="#">Downloads</a></li>
+                <li><a href="#">About</a></li>
+                <li><a href="#">Contact</a></li>
+              </ul>
+            </div>
+
+
+
