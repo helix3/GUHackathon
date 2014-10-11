16ab03345d7202339f96b80466bf55b8ddeb7038
rokasl <rokas@helix.lt>
2014-10-11 19:06:55 +0100

    added bootstrap/css and updated index page

--- app/views/hack/theme/stats.blade.php
+++ app/views/hack/theme/stats.blade.php
@@ -14,7 +14,7 @@
       <div class="col-md-2"></div>
       <div class="col-md-8">
 
-        <div class="thumbnail">
+        <div class="thumbnail"> <style>.thumbnail { opacity:0.8; }</style>
         <h3>
             Terorism acts since 1970 till now: <?= \Hack\SasList::count() ?>
         </h3>
