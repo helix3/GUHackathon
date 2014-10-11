32a5c29cc81f7d06b64a65af10e8703d72dcc0e2
rokasl <rokas@helix.lt>
2014-10-11 08:00:02 +0100

    added bootstrap/css and updated index page

--- app/views/hack/theme/index.blade.php
+++ app/views/hack/theme/index.blade.php
@@ -92,6 +92,20 @@
     );
 
 
+<!--FANCYBOX JS -->
+$(document).ready(function() {
+	$(".various").fancybox({
+		maxWidth	: 800,
+		maxHeight	: 600,
+		fitToView	: false,
+		width		: '70%',
+		height		: '70%',
+		autoSize	: false,
+		closeClick	: false,
+		openEffect	: 'none',
+		closeEffect	: 'none'
+	});
+});
 </script>
 
 
