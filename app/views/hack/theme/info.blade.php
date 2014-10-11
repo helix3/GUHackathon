35451e90456590a8db0430e588c304b109e5e3d9
rokasl <rokas@helix.lt>
2014-10-11 07:34:39 +0100

    added bootstrap/css and updated index page

---
+++ app/views/hack/theme/info.blade.php
@@ -0,0 +1,97 @@
+    <!DOCTYPE html>
+       <html lang="en">
+         <head>
+           <meta charset="utf-8">
+           <meta http-equiv="X-UA-Compatible" content="IE=edge">
+           <meta name="viewport" content="width=device-width, initial-scale=1">
+
+           <!-- Add jQuery library -->
+           <script type="text/javascript" src="http://code.jquery.com/jquery-latest.min.js"></script>
+
+           <!-- Add mousewheel plugin (this is optional) -->
+           <script type="text/javascript" src="assets/fancybox/lib/jquery.mousewheel-3.0.6.pack.js"></script>
+
+           <!-- Add fancyBox -->
+           <link rel="stylesheet" href="assets/fancybox/source/jquery.fancybox.css?v=2.1.5" type="text/css" media="screen" />
+           <script type="text/javascript" src="assets/fancybox/source/jquery.fancybox.pack.js?v=2.1.5"></script>
+
+           <!-- Optionally add helpers - button, thumbnail and/or media -->
+           <link rel="stylesheet" href="assets/fancybox/source/helpers/jquery.fancybox-buttons.css?v=1.0.5" type="text/css" media="screen" />
+           <script type="text/javascript" src="assets/fancybox/source/helpers/jquery.fancybox-buttons.js?v=1.0.5"></script>
+           <script type="text/javascript" src="assets/fancybox/source/helpers/jquery.fancybox-media.js?v=1.0.6"></script>
+
+           <link rel="stylesheet" href="assets/fancybox/source/helpers/jquery.fancybox-thumbs.css?v=1.0.7" type="text/css" media="screen" />
+           <script type="text/javascript" src="assets/fancybox/source/helpers/jquery.fancybox-thumbs.js?v=1.0.7"></script>
+
+           <title>World Terrorism Database</title>
+
+           <!-- Bootstrap core CSS -->
+           <link href="/assets/bootstrap-3.2.0-dist/css/bootstrap.min.css" rel="stylesheet">
+
+           <!-- Custom styles for this template -->
+           <link href="assets/css/justified-nav.css" rel="stylesheet">
+
+           <!-- HTML5 shim and Respond.js IE8 support of HTML5 elements and media queries -->
+           <!--[if lt IE 9]>
+             <script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
+             <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
+           <![endif]-->
+         </head>
+
+         <body>
+
+
+         <table class="table table-hover">
+         <thead>
+           <tr>
+              <th>Date</th>
+              <th>Location</th>
+              <th>Attack type</th>
+              <th>Target</th>
+              <th>Nationality of the target</th>
+              <th>Terrorist organization</th>
+              <th>Motive</th>
+              <th>Weapon</th>
+              <th>Killed/injured</th>
+              <th>Property damage</th>
+              <th>Notes</th>
+
+           </tr> 
+         </thead>
+         <tbody>
+           <tr>
+                       <td></td>
+                       <td></td>
+                       <td></td>
+                       <td></td>
+                       <td></td>
+                       <td></td>
+                       <td></td>
+                       <td></td>
+                       <td></td>
+                       <td></td>
+                       <td></td>
+         
+                    </tr> 
+         
+         
+         </tbody>
+         
+
+         </table>
+
+
+
+
+
+
+
+
+
+   <!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.1/jquery.min.js"></script>
+    <!-- Include all compiled plugins (below), or include individual files as needed -->
+    <script src="assets/js/bootstrap.min.js"></script>
+
+    </body>
+    </html>
